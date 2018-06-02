b33ff9f231d91a751e6d4a6010cf03b7c0d9da62
Michael Scott <33076411+Creeper37500-da-Snek@users.noreply.github.com>
2018-06-01 23:39:24 -0400

    Update v1.5.

--- UniversalCraft/UniversalCraft.cs
+++ UniversalCraft/UniversalCraft.cs
@@ -12,32 +12,5 @@ namespace UniversalCraft
                 Autoload = true,
             };
         }
-
-        /*public static bool SacredToolsLoaded = ModLoader.GetMod("SacredTools") != null;
-
-
-
-        public static Mod Thorium;
-        public static Mod SacredTools;
-        public static Mod Calamity;
-        public static Mod Tremor;
-
-        public override void Load()
-        {
-            Thorium = ModLoader.GetMod("ThoriumMod");
-            SacredTools = ModLoader.GetMod("SacredTools");
-            Calamity = ModLoader.GetMod("CalamityMod");
-            Tremor = ModLoader.GetMod("Tremor");
-
-            AddGlobalTile("UCTile", new UCTile());
-        }
-
-        public override void Unload()
-        {
-            Thorium = null;
-            SacredTools = null;
-            Calamity = null;
-            Tremor = null;
-        }*/
     }
 }
