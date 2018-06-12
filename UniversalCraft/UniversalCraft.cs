94043af1a5415260ca8914f4822c2768edb60b9e
Michael Scott <33076411+Creeper37500-da-Snek@users.noreply.github.com>
2018-06-12 17:16:46 -0400

    Add files via upload

--- UniversalCraft/UniversalCraft.cs
+++ UniversalCraft/UniversalCraft.cs
@@ -1,5 +1,5 @@
+using Terraria.ID;
 using Terraria.ModLoader;
-using UniversalCraft.Tiles;
 
 namespace UniversalCraft
 {
@@ -11,6 +11,27 @@ namespace UniversalCraft
             {
                 Autoload = true,
             };
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(this);
+            recipe.AddIngredient(ItemID.EmptyBucket, 1);
+            recipe.needWater = true;
+            recipe.SetResult(ItemID.WaterBucket, 1);
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(this);
+            recipe.AddIngredient(ItemID.EmptyBucket, 1);
+            recipe.needLava = true;
+            recipe.SetResult(ItemID.LavaBucket, 1);
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(this);
+            recipe.AddIngredient(ItemID.EmptyBucket, 1);
+            recipe.needHoney = true;
+            recipe.SetResult(ItemID.HoneyBucket, 1);
+            recipe.AddRecipe();
         }
     }
 }
